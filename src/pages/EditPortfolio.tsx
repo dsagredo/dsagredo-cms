b3ac47b786353d55006125a73364f936e52bf08f
Diego Sagredo <dsagredog@gmail.com>
2025-10-08 12:18:54 -0300

    Updated EditPortfolio.tsx

--- src/pages/EditPortfolio.tsx
+++ src/pages/EditPortfolio.tsx
@@ -143,8 +143,8 @@ const EditPost: FC = (): JSX.Element => {
                         </div>
 
                         <div>
-                            <label className="block text-sm font-medium text-gray-700 mb-1">
-                                Content
+                            <label className="block text-sm font-medium text-slate-300 mb-1">
+                                Descripción
                             </label>
                             <textarea
                                 className="w-full h-64 px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
